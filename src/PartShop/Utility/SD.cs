f4867b6ff1a435f2207b49a80ddcd8750a05a69b
KrusteF <79479041+KrusteF@users.noreply.github.com>
2021-06-13 00:09:28 +0300

    Add products + views + partials

---
+++ src/PartShop/Utility/SD.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+using PartShop.Models;
+
+namespace PartShop.Utility
+{
+    public static class SD
+    {
+        public const string DefaultToolImage = "default_tool.jpg";
+
+        public const string AdminUser = "Admin";
+        public const string CustomerUser = "Customer";
+        public const string ssShoppingCartCount = "ssCartCount";
+        public const string ssCouponCode = "ssCouponCode";
+
+        public const string StatusSubmitted = "Submitted";
+        public const string StatusInProcess = "Being Prepared";
+        public const string StatusReady = "Ready for Pickup";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusRejected = "Rejected";
+
+        public const string OrderPlacementStatus = "OrderPlaced.png";
+        public const string OrderAcceptedStatus = "InKitchen.png";
+        public const string ReadyForPickupStatus = "ReadyForPickup.png";
+        public const string CompletedStatus = "completed.png";
+
+        public static string ConvertToRawHtml(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
+
+        public static double DiscountPrice(Coupon couponFromDb, double OrignalOrderTotal)
+        {
+            if (couponFromDb == null)
+            {
+                return OrignalOrderTotal;
+            }
+            else
+            {
+                if (couponFromDb.MinimumAmount > OrignalOrderTotal)
+                {
+                    return OrignalOrderTotal;
+                }
+                else
+                {
+                    if (Convert.ToInt32(couponFromDb.CouponType) == (int)Coupon.ECouponType.Сума)
+                    {
+                        return Math.Round(OrignalOrderTotal - couponFromDb.Discount, 2);
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(couponFromDb.CouponType) == (int)Coupon.ECouponType.Процент)
+                        {
+                            return Math.Round(OrignalOrderTotal - (OrignalOrderTotal * couponFromDb.Discount / 100), 2);
+                        }
+                    }
+                }
+            }
+            return OrignalOrderTotal;
+        }
+
+    }
+}
---
+++ src/PartShop/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using PartShop.Data;
+
+namespace PartShop.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _db.ApplicationUser.FirstOrDefaultAsync(p => p.Id == claim.Value);
+            return View(userFromDb);
+        }
+    }
+}
